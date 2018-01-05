714800a73737cbcf6db8d827be8ee15371f448f6
nojmana <neumannkinga7@gmail.com>
2018-01-05 21:13:47 +0100

    #5262 Add tests for all special characters, no characters and a few characters in a sentence

--- tests/pl/put/poznan/transformer/logic/TestTextTransformerLatex.java
+++ tests/pl/put/poznan/transformer/logic/TestTextTransformerLatex.java
@@ -42,4 +42,19 @@ public class TestTextTransformerLatex {
 	public void testToLatexDollar() {
 		assertEquals("\\$", textTransformer.toLatex("$"));
 	}
+	
+	@Test
+	public void testToLatexOnlySpecialCharacters() {
+		assertEquals("\\%\\_\\{\\}\\#\\&\\$", textTransformer.toLatex("%_{}#&$"));
+	}
+	
+	@Test
+	public void testToLatexStringNoCharacters() {
+		assertEquals("Example without any special character", textTransformer.toLatex("Example without any special character"));
+	}
+	
+	@Test
+	public void testToLatexStringCharacters() {
+		assertEquals("Exa\\{mple with\\_special\\%characters", textTransformer.toLatex("Exa{mple with_special%characters"));
+	}
 }
